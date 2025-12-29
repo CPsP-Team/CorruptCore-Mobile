bf50d273035f090f0d1359fab3716d9fd58e457c
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-12-29 16:43:20 +0200

    idk man

--- source/Main.hx
+++ source/Main.hx
@@ -70,6 +70,8 @@ class Main extends Sprite
 		MobileUtil.initDirectory(); //do not make this jobs everytime
 		MobileUtil.getPermissions();
 		MobileUtil.copySpesificFileFromAssets('mobile/storageModes.txt', MobileUtil.getCustomStoragePath());
+		if (!MobileUtil.areAssetsCopied("assets/"))
+			MobileUtil.copyAssetsFromAPK("assets/");
 		#end
 		Sys.setCwd(MobileUtil.getStorageDirectory());
 		#end
