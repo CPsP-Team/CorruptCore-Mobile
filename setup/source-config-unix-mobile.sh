77a963d7df6193948f669a0e5cc97912816bce40
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-12-31 11:46:22 +0500

    Just will see if ma lime works for android

--- setup/source-config-unix-mobile.sh
+++ setup/source-config-unix-mobile.sh
@@ -10,7 +10,7 @@ haxelib install tjson --quiet
 haxelib install hxjsonast --quiet
 haxelib install flixel 6.1.2
 haxelib set flixel 6.1.2
-haxelib git lime https://github.com/MaysLastPlays-Stuff/lime-cc
+haxelib git lime https://github.com/GreenColdTea/lime
 haxelib git openfl https://github.com/GreenColdTea/openfl.git
 haxelib install format --quiet
 haxelib install hxp --quiet
